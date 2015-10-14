92094268b625b68554f043f939e418564935dc22
Thomas Hopkins <thopkins@redventures.com>
2015-10-14 15:52:15 -0400

    update example for api

--- examples/main.go
+++ examples/main.go
@@ -1,12 +1,10 @@
 package main
 
 import "github.com/hopkinsth/lambda-phage"
-import "fmt"
 
 func main() {
 	ch := lambda.Listen()
 	for r := range ch {
-		fmt.Println(r.Context.AwsRequestID)
-		r.ResponseData <- nil
+		r.Succeed("yay")
 	}
 }
